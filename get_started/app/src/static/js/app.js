724b7063f437baa508d82bdad6c19c43643d45e0
Andrzej Połetek <andrzej@poletek.net>
2022-05-13 17:39:02 +0200

    [PART 3]

--- get_started/app/src/static/js/app.js
+++ get_started/app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">You dont have items to do! Add new!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
